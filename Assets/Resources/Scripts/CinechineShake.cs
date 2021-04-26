efea73e5029fcc3534cb7fe8e10ae15d4f5e7d6c
luisgkingeski <44435379+luisgkingeski@users.noreply.github.com>
2021-04-26 12:48:19 -0300

    add resources folder

---
+++ Assets/Resources/Scripts/CinechineShake.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using Cinemachine;
+using System.Collections;
+
+public class CinechineShake : MonoBehaviour
+{
+    public static CinechineShake Instance { get; private set; }
+
+    private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float shakeTimer = 0;
+    //  private float shakeTimerTotal;
+    // private float startingIntersity;
+
+
+    private void Start()
+    {
+        Instance = this;
+        cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+
+    public void ShakeCamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        // startingIntersity = intensity;
+        // shakeTimerTotal = time;
+        shakeTimer = time;
+    }
+
+    private void Update()
+    {
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0f)
+            {
+                CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                           cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0;
+                //cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 
+                //    Mathf.Lerp(startingIntersity, 0f, shakeTimer / shakeTimerTotal);                 
+            }
+        }
+    }
+}
