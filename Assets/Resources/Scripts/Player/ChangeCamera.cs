84217ade6d2894403eef43d4852c82ead36750df
luisgkingeski <44435379+luisgkingeski@users.noreply.github.com>
2021-04-26 17:16:10 -0300

    Add objects and scripts of movement and camera

---
+++ Assets/Resources/Scripts/Player/ChangeCamera.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ChangeCamera : SingletonMonobehaviour<ChangeCamera>
+{
+    public GameObject cinemachineCamera;
+    public GameObject head;
+    public GameObject mainCamera;
+
+    public CinemachineVirtualCamera pov;
+    public CinemachineVirtualCamera puzzle1;
+    public CinemachineVirtualCamera puzzle2;
+    public CinemachineVirtualCamera puzzle3;
+
+
+    private void Start()
+    {
+        cinemachineCamera.SetActive(false);
+    }
+
+    public void StartPuzzle1()
+    {
+        StartCoroutine(Puzzle1());
+    }
+
+    public void StartPuzzle2()
+    {
+        StartCoroutine(Puzzle2());
+    }
+
+    public void StartPuzzle3()
+    {
+        StartCoroutine(Puzzle3());
+    }
+
+
+    public IEnumerator ReturnPOV()
+    {
+        pov.m_Priority = 10;
+        puzzle1.m_Priority = 0;
+        puzzle2.m_Priority = 0;
+        puzzle3.m_Priority = 0;
+        yield return new WaitForSeconds(1);
+        head.transform.parent = mainCamera.transform;
+        mainCamera.SetActive(true);
+        cinemachineCamera.SetActive(false);
+    }
+
+    public IEnumerator Puzzle1()
+    {
+        head.transform.parent = null;
+        mainCamera.SetActive(false);
+        cinemachineCamera.SetActive(true);
+        pov.m_Priority = 10;
+        yield return new WaitForSeconds(1);
+        pov.m_Priority = 9;
+        puzzle1.m_Priority = 10;
+    }
+
+    public IEnumerator Puzzle2()
+    {
+        head.transform.parent = null;
+        mainCamera.SetActive(false);
+        cinemachineCamera.SetActive(true);
+        pov.m_Priority = 10;
+        yield return new WaitForSeconds(1);
+        pov.m_Priority = 9;
+        puzzle2.m_Priority = 10;
+    }
+
+    public IEnumerator Puzzle3()
+    {
+        head.transform.parent = null;
+        mainCamera.SetActive(false);
+        cinemachineCamera.SetActive(true);
+        pov.m_Priority = 10;
+        yield return new WaitForSeconds(1);
+        pov.m_Priority = 9;
+        puzzle3.m_Priority = 10;
+    }
+
+
+}
+
---
+++ Assets/Resources/Scripts/Player/Player.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public GameObject mainCamera;
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+    private ChangeCamera changeCamera;
+
+    private void Start()
+    {
+        changeCamera = ChangeCamera.Instance;
+    }
+
+    void FixedUpdate()
+    {
+        if (mainCamera.activeInHierarchy)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed);
+
+        velocity.y += gravity;
+
+        controller.Move(velocity * (Time.fixedDeltaTime * Time.fixedDeltaTime));
+    }
+
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (other.gameObject.layer == LayerMask.NameToLayer("Puzzle1"))
+            {
+                changeCamera.StartPuzzle1();
+            }
+            else if (other.gameObject.layer == LayerMask.NameToLayer("Puzzle2"))
+            {
+                changeCamera.StartPuzzle2();
+            }
+            else if (other.gameObject.layer == LayerMask.NameToLayer("Puzzle3"))
+            {
+                changeCamera.StartPuzzle3();
+            }
+        }
+    }
+
+}
