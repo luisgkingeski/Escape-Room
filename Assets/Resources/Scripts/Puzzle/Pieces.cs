9c416ed45423f05f4060314e7304e43d4e93ca18
luisgkingeski <44435379+luisgkingeski@users.noreply.github.com>
2021-04-27 15:07:17 -0300

    Add puzzle1 logic

---
+++ Assets/Resources/Scripts/Puzzle/Pieces.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Pieces : MonoBehaviour
+{
+    public int pieceIndex;
+    public LayerMask layerMask;
+    public bool canMove = false;
+    private Puzzle1 p;
+    private void Start()
+    {
+        p = Puzzle1.Instance;
+        StartCoroutine(GetIndex());
+    }
+
+
+    private void Update()
+    {
+        if (pieceIndex != 0)
+        {
+            canMove = false;
+            Raycasts();
+        }
+    }
+
+    private void Raycasts()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up, out hit, 100, layerMask))
+        {
+            if (hit.transform.gameObject.name == "0")
+            {
+                canMove = true;
+            }
+            else
+            {
+                canMove = false;
+            }
+        }
+
+        if (canMove)
+        {
+            return;
+        }
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 100, layerMask))
+        {
+            if (hit.transform.gameObject.name == "0")
+            {
+                canMove = true;
+            }
+            else
+            {
+                canMove = false;
+            }
+        }
+
+        if (canMove)
+        {
+            return;
+        }
+
+        if (Physics.Raycast(transform.position, transform.right, out hit, 100, layerMask))
+        {
+            if (hit.transform.gameObject.name == "0")
+            {
+                canMove = true;
+            }
+            else
+            {
+                canMove = false;
+            }
+        }
+
+        if (canMove)
+        {
+            return;
+        }
+
+        if (Physics.Raycast(transform.position, -transform.right, out hit, 100, layerMask))
+        {
+            if (hit.transform.gameObject.name == "0")
+            {
+                canMove = true;
+            }
+            else
+            {
+                canMove = false;
+            }
+        }
+    }
+
+    IEnumerator GetIndex()
+    {
+        yield return new WaitForSeconds(0.1f);
+        pieceIndex = int.Parse(gameObject.name);
+    }
+
+    private void OnMouseDown()
+    {
+        if (canMove)
+        {
+            p.MovePiece(pieceIndex);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Puzzle/Puzzle1.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1 : SingletonMonobehaviour<Puzzle1>
+{
+    public List<GameObject> piecesList;
+    public GameObject pieces;
+    public float speed = 2f;
+    
+    void Start()
+    {
+        piecesList = new List<GameObject>();
+
+        int i = 1;
+        foreach (Transform child in pieces.transform.GetChild(0).transform)
+        {
+            child.gameObject.name = i.ToString();
+            piecesList.Add(child.gameObject);
+            i++;
+        }
+        piecesList[i - 2].gameObject.name = "0";
+    }
+
+    public void MovePiece(int index)
+    {
+        GameObject target = piecesList[index - 1];
+        Vector3 aux = target.transform.position;
+        GameObject empty = piecesList[piecesList.Count - 1];
+
+
+        StartCoroutine(StartMovePiece(target, target.transform.position, empty.transform.position));
+        StartCoroutine(MoveEmptyPiece(empty, empty.transform.position, aux));
+    }
+
+    IEnumerator StartMovePiece(GameObject target, Vector3 start, Vector3 destiny)
+    {
+        var t = 0f;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime * speed;
+
+            if (t > 1)
+            {
+                t = 1;
+            }
+
+            target.transform.position = Vector3.Lerp(start, destiny, t);
+
+            yield return null;
+        }
+    }
+
+    IEnumerator MoveEmptyPiece(GameObject target, Vector3 start, Vector3 destiny)
+    {
+        var t = 0f;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime * speed;
+
+            if (t > 1)
+            {
+                t = 1;
+            }
+
+            target.transform.position = Vector3.Lerp(start, destiny, t);
+
+            yield return null;
+        }
+    }
+
+}
