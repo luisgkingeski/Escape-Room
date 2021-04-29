f4a6585ae6aa2c6e65fef0522a54dd1e69b145f0
luisgkingeski <44435379+luisgkingeski@users.noreply.github.com>
2021-04-29 15:33:02 -0300

    Add puzzle3

---
+++ Assets/Resources/Scripts/Puzzle/Puzzle3/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Puzzle3"))
+        {
+            Puzzle3.Instance.Win();
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Puzzle3"))
+        {
+            Puzzle3.Instance.Win();
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Puzzle/Puzzle3/Puzzle3.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Puzzle3 : SingletonMonobehaviour<Puzzle3>
+{
+    public MeshRenderer pointSphere;
+    public Perspective perspective;
+    public bool win = false;
+
+    public void Win()
+    {
+        pointSphere.material.EnableKeyword("_EMISSION");
+        perspective.enabled = false;
+        win = true;
+    }
+}
